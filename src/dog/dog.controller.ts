bbb76c780b9ff1a42b7076521b80b66583fd8ab9
Joseph <kamoldinovq@gmail.com>
2024-11-22 18:56:03 +0900

    BRR: Initial Setup

---
+++ src/dog/dog.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { DogService } from './dog.service';
+import {Request, query} from 'express'
+
+@Controller('dog')
+export class DogController {
+    constructor(private readonly dogService: DogService) {}
+
+    @Get()
+    public getHello(): string {
+        return this.dogService.getHello();
+    }
+
+    @Get("greet/:id")
+    public introduce(@Param() params: string, @Query() query: any): string {
+        console.log('params:', params);
+        console.log('query:', query);
+        return this.dogService.introduce();
+    }
+}
+
+
+
---
+++ src/dog/dog.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DogService {
+    public getHello(): string {
+        return "WOW-WOW";
+    }
+
+    public introduce(): string {
+        return 'My name is Rex!';
+        
+    }
+}
